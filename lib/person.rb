bf8df4bbb1ee77758fcb0e9da49eb063f1cd89d0
Lisa Feng <lisafeng10@gmail.com>
2020-06-23 14:14:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
   def name=(person_name)
-    @this_persons_name = person_name
+    @name = person_name
   end
   
   def name
-    @this_persons_name
+    @name
   end
   
   def job=(a_job)
-    @this_persons_job = a_job
+    @job = a_job
   end
   
   def job
-    @this_persons_job
+    @job
   end
 end
